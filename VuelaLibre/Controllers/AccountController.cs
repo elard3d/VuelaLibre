6cd3fe00e5c7ed2eca9a17ff9854f6a801670bd9
Riosan575 <an_99_ben@hotmail.com>
2020-09-22 14:32:44 -0500

    Nuevo

--- VuelaLibre/Controllers/AccountController.cs
+++ VuelaLibre/Controllers/AccountController.cs
@@ -3,25 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VuelaLibre.Models;
 
 namespace VuelaLibre.Controllers
 {
     public class AccountController : Controller
     {
-        public ViewResult Index()
+        private VuelaLibreContext _context;
+        public AccountController(VuelaLibreContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
         {
             return View("Index");
         }
 
-        public ViewResult Register()
+
+        [HttpGet]
+        public ActionResult Register() // GET
         {
-            return View("Register");
+            return View("Register", new Account());
+        }
+
+        [HttpPost]
+        public ActionResult Register(Account account) // POST
+        {
+            //if(account.Name == null || account.Name == "")
+            //    ModelState.AddModelError("Name1", "El campo Nombre es requerido");
+
+            if (ModelState.IsValid)
+            {
+                _context.Accounts.Add(account);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("Register", account);
         }
 
         public ViewResult Login()
         {
             return View("Login");
         }
+
+        
 
     }
 }
--- VuelaLibre/Startup.cs
+++ VuelaLibre/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VuelaLibre.Models;
 
 namespace VuelaLibre
 {
@@ -24,6 +26,10 @@ namespace VuelaLibre
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<VuelaLibreContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +56,7 @@ namespace VuelaLibre
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Index}/{id?}");
             });
         }
     }
