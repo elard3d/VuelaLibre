85a87a5a70d373f2ea6bb3ce6a4d2fc274907c5c
Riosan575 <an_99_ben@hotmail.com>
2020-10-15 11:56:53 -0500

    nuevo

--- VuelaLibre/Models/Vuelo.cs
+++ VuelaLibre/Models/Vuelo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
@@ -11,21 +12,21 @@ namespace VuelaLibre.Models
     public class Vuelo
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo origen es obligatorio")]
         public string Origen { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo destinos es obligatorio")]
         public string Destino { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo aerolinea es obligatorio")]
         public string Aerolinea { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo fechaida es obligatorio")]
         public DateTime FechaIda { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo fecharegreso es obligatorio")]
         public DateTime FechaRegreso { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo tiempo de vuelo es obligatorio")]
         public int TiempoVuelo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo total pasajes es obligatorio")]
         public int TotalPasaje { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo precio es obligatorio")]
         public decimal Precio { get; set; }
     }
 }
